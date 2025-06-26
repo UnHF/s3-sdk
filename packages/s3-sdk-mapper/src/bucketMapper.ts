89a72b8e48f8d037cb4463276d4bdbee1ba14163
heyongqi10 <heyongqi10@jd.com>
2025-06-26 16:46:02 +0800

    feat: 新增getService的 mapper

--- packages/s3-sdk-mapper/src/bucketMapper.ts
+++ packages/s3-sdk-mapper/src/bucketMapper.ts
@@ -1,3 +1,30 @@
-export class BucketMapper {
-    
+import S3SDK from '@unhf/s3-sdk-abstract';
+import type { Bucket } from '@unhf/s3-sdk-models';
+import { createSignature } from "@unhf/s3-sdk-signature";
+
+export default class BucketMapper {
+    constructor(private s3: S3SDK) {}
+
+    async getService () {
+        const url = '/'
+        const date = new Date().toUTCString();
+        const signature = createSignature(
+            'GET',
+            '',
+            '',
+            date,
+            '',
+            url,
+            this.s3.secretKey
+        )
+        const authorization = `jingdong ${this.s3.accessKey}:${signature}`;
+        return await this.s3.axiosInstance.get<{
+            Buckets: Bucket[];
+        }>(url, {
+            headers: {
+                Authorization: authorization,
+                Date: date,
+            }
+        }).catch()
+    }
 }
