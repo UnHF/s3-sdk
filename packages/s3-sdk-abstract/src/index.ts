3e831c4ab5c68cf96fa746dd8213a1ac320f0456
heyongqi10 <heyongqi10@jd.com>
2025-06-26 16:46:17 +0800

    feat: 新增抽象层

--- packages/s3-sdk-abstract/src/index.ts
+++ packages/s3-sdk-abstract/src/index.ts
@@ -1,13 +1,9 @@
 import { type AxiosInstance } from 'axios';
-import { type GetObjectVO } from './vo';
 
 abstract class S3SDK {
     abstract accessKey: string;
     abstract secretKey: string;
     abstract axiosInstance: AxiosInstance;
-
-    abstract getService (): Promise<GetObjectVO>
 }
 
 export default S3SDK;
-export { type GetObjectVO };
--- packages/s3-sdk-abstract/src/vo.ts
+++
@@ -1,5 +0,0 @@
-import { type Bucket } from "@unhf/s3-sdk-models";
-
-export interface GetObjectVO {
-    Buckets: Bucket[];
-}
